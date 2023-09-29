632759ee6e949f397cda8d6d988364c764396b0c
LAPTOP-EIABQG0H\tulip <tasha@xquant-ai.com>
2023-09-29 16:51:17 +0900

    fin

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ pygame.init()
 title_font = pygame.font.Font(None, 40)
 score_surface = title_font.render("Score", True, Colors.white)
 next_surface = title_font.render("Next", True, Colors.white)
-game_over_surface = title_font.render("Game Over", True, Colors.white)
+start_surface = title_font.render("Start", True, Colors.white)
+start_rect = pygame.Rect(320, 440, 170, 50)
 press_space_surface = title_font.render("Press Space", True, Colors.white)
 score_rect = pygame.Rect(320, 55, 170, 50)
 next_rect = pygame.Rect(320, 215, 170, 180)
 
+# ####### 시작 버튼 정의 ########
+# start_button_surface = title_font.render("Start", True, Colors.white)
+# start_button = pygame.Rect(200, 300, 100, 50)
 
 ####### 게임 스크린 정의 ########
 screen = pygame.display.set_mode((500, 620)) # width, height
@@ -24,6 +28,14 @@ clock = pygame.time.Clock()
 game = Game()
 GAME_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(GAME_UPDATE, 300)
+
+### 버튼
+
+
+
+##게임 시작 기본 상태
+game.game_over = True
+game.grid.reset()
 
 while True:
     ######### 이벤트 처리 #########
@@ -51,6 +63,12 @@ while True:
         if event.type == GAME_UPDATE and (not game.game_over): # 0.2초마다 블록이 한칸씩 내려감
             game.move_down()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
+            if start_rect.collidepoint(mouse_pos):
+                game.game_over = False
+                game.reset()
+
     ######### 화면 그리기 #########
     score_value_surface = title_font.render(str(game.score), True, Colors.white)
 
@@ -58,8 +76,9 @@ while True:
     screen.blit(score_surface, (365, 20,50, 50))
     screen.blit(next_surface, (370, 170,50, 50))
     if game.game_over:
-        screen.blit(game_over_surface, (330, 450, 50, 50))
+        screen.blit(start_surface, (370, 450, 50, 50))
         screen.blit(press_space_surface, (325, 500, 50, 50))
+        pygame.draw.rect(screen, Colors.white, start_rect, 5)
     pygame.draw.rect(screen, Colors.light_blue, score_rect, 5)
     screen.blit(score_value_surface, score_value_surface.get_rect(centerx = score_rect.centerx, centery = score_rect.centery))
     pygame.draw.rect(screen, Colors.light_blue, next_rect, 5)
